ded29913da60eb83b9708aa9304a85fd81ddffa1
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-07 09:08:30 -0500

    seed typo fixed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -3,20 +3,20 @@ const cars = [
         vin: '37465736473647364',
         make: 'Mercedes',
         model: 'GT',
-        mielage: 34000,
+        mileage: 34000,
         title: 'clean'
     },
     {
         vin: '73657364738273647',
         make: 'Acura',
         model: 'TL',
-        mielage: 234789
+        mileage: 234789
     },
     {
         vin: '73647263746372637',
         make: 'BMW',
         model: 'M4',
-        mielage: 3000,
+        mileage: 3000,
         title: 'salvage',
         transmission: 'manual'
     },
@@ -24,7 +24,7 @@ const cars = [
         vin: '92837465738473829',
         make: 'Infinity',
         model: 'Q60',
-        mielage: 12000,
+        mileage: 12000,
         transmission: 'automatic'
     }
 ]
