9d6abede62e037a358987708bc39f185cb4ad207
Art Vardanyan <akvardanyan99@gmail.com>
2023-11-07 05:42:44 -0500

    up migration logic

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,20 @@
+const { default: knex } = require("knex");
+@returns {Promise<void>}
+
+
+
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  await knex.schema.createTable('dealer', table => {
+    table.increments('id')
+    table.string('vin', 17).notNullable().unique()
+    table.string('make', 128).notNullable()
+    table.string('model', 128).notNullable()
+    table.numeric('mileage').unsigned().notNullable()
+    table.string('title', 128)
+    table.string('transmission', 128)
+  })
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+
 };
