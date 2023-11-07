74218f4af211aa328b927ee8ed2c3249c87bf716
Art <126132705+artomatiq@users.noreply.github.com>
2023-11-07 16:59:27 -0500

    Update cars-middleware.js

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,6 @@
 const db = require('../../data/db-config')
 const Cars = require('./cars-model')
+const vin = require('vin-validator');
 
 const checkCarId = async (req, res, next) => {
   const car = await Cars.getById(req.params.id)
@@ -17,15 +18,49 @@ const checkCarId = async (req, res, next) => {
 }
 
 const checkCarPayload = async (req, res, next) => {
+  const {vin, make, model, mileage} = req.body
+  const error = {status: 400}
+
+  if (!vin) {
+    error.message = 'vin is missing'
+  }
+  if (!make) {
+    error.message = `make is missing`
+  }
+  if (!model) {
+    error.message = `model is missing`
+  }
+  if (!mileage) {
+    error.message = `mileage is missing`
+  }
+  if (error.message) {
+    next(error)
+  }
+  else {
+    next()
+  }
+
 
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  const vinValid = vin.validate(req.body.vin)
+
+  if (vinValid) {
+    next()
+  }
+  else {
+    next({status: 400, message: `vin ${req.body.vin} is invalid`})
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  const exists = await Cars.getByVin(req.body.vin)
+  
+  if (exists) {
+    next({status:400, message: `vin ${req.body.vin} already exists`})
+  }
+  else next()
 }
 
 module.exports = {
